2e5992dcc5ed916e872fad9ce681dcecc66afb63
FrankLikuohao <frank.likuohao@gmail.com>
2016-07-26 10:54:18 +0800

    edit Text

--- SimpleUI/app/src/main/java/com/example/user/simpleui/MainActivity.java
+++ SimpleUI/app/src/main/java/com/example/user/simpleui/MainActivity.java
@@ -2,12 +2,27 @@ package com.example.user.simpleui;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+      TextView textView;
+    EditText editText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textView = (TextView)findViewById(R.id.textView);
+        editText = (EditText)findViewById(R.id.editText);
+    }
+
+    public void submit(View view)
+    {
+        String text=editText.getText().toString();
+        textView.setText(text);
+    editText.setText("");
     }
 }
