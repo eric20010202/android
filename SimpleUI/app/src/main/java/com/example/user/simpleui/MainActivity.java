8eb7da4f8067471429fb56e0beb12478e064630a
eric20010202 <eric20010202@icloud.com>
2016-07-27 12:36:23 +0800

    snack bar

--- SimpleUI/app/src/main/java/com/example/user/simpleui/MainActivity.java
+++ SimpleUI/app/src/main/java/com/example/user/simpleui/MainActivity.java
@@ -1,20 +1,34 @@
 package com.example.user.simpleui;
 
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.KeyEvent;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.RadioButton;
 import android.widget.RadioGroup;
+import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
-      TextView textView;
+    TextView textView;
     EditText editText;
     RadioGroup radioGroup;
+    ListView listView;
+    Spinner spinner;
 
-    String sex="male";
+    String drink = "black tea";
+
+    List<Order> orders = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,18 +37,18 @@ public class MainActivity extends AppCompatActivity {
 
         textView = (TextView)findViewById(R.id.textView);
         editText = (EditText)findViewById(R.id.editText);
-        radioGroup=(RadioGroup)findViewById(R.id.radioGroup);
+        radioGroup = (RadioGroup)findViewById(R.id.radioGroup);
+        listView = (ListView)findViewById(R.id.listView);
+        spinner = (Spinner)findViewById(R.id.spinner);
 
         radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(RadioGroup group, int checkedId) {
-                if(checkedId ==R.id.maleradioButton)
-                {
-                    sex ="male";
-                }
-                else if (checkedId ==R.id.femaleradioButton);
+                RadioButton radioButton = (RadioButton)group.findViewById(checkedId);
+                drink = radioButton.getText().toString();
             }
         });
+
         editText.setOnKeyListener(new View.OnKeyListener() {
             @Override
             public boolean onKey(View v, int keyCode, KeyEvent event) {
@@ -45,14 +59,55 @@ public class MainActivity extends AppCompatActivity {
                 return false;
             }
         });
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Order order = (Order)parent.getAdapter().getItem(position);
+//                Toast.makeText(MainActivity.this, "You click on" + order.note, Toast.LENGTH_SHORT).show();
+                Snackbar.make(parent, "You click on" + order.note, Snackbar.LENGTH_SHORT).setAction("OK", new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+
+                    }
+                }).show();
+            }
+        });
+
+        setupListView();
+        setupSpinner();
+
     }
 
+    private void setupListView()
+    {
+//        String[] data = new String[]{"1","2","3",};
+//        ArrayAdapter adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, orders);
+
+        OrderAdapter adapter = new OrderAdapter(this, orders);
+        listView.setAdapter(adapter);
+    }
+
+    private void setupSpinner()
+    {
+        String[] data = getResources().getStringArray(R.array.storeInfos);
+        ArrayAdapter adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, data);
+        spinner.setAdapter(adapter);
+    }
 
     public void submit(View view)
     {
-        String text=editText.getText().toString();
-        text = text +"性別:"+ sex;
-        textView.setText(text);
-    editText.setText("");
+        String text = editText.getText().toString();
+        String result = text + " order: " + drink;
+        textView.setText(result);
+        editText.setText("");
+
+        Order order = new Order();
+        order.note = text;
+        order.drink = drink;
+        order.storeInfo = (String)spinner.getSelectedItem();
+
+        orders.add(order);
+        setupListView();
     }
 }
